1ec42c952a2ae7b5afc8781cc046286e46cb612f
toesteatoes <ddyt904@gmail.com>
2023-10-15 20:35:43 -0700

    UI complete. forgot to commit small changes

--- thegame.js
+++ thegame.js
@@ -1,7 +1,10 @@
 console.log("Hello world, and welcome to rock paper scissors.");
 
+
+//GAME LOGIC 
 let playerScore = 0;
 let compScore = 0;
+let gameEnded = false;
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -29,41 +32,106 @@ function getComputerChoice () {
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     console.log("tie");
+    result.textContent = "Round tied!";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {   
     playerScore++;
+    result.textContent = "Player wins round!";
     console.log("player gets point. total: " + playerScore);
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerScore++;
+    result.textContent = "Player wins round!";
     console.log("player gets point. total: " + playerScore);
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore++;
+    result.textContent = "Player wins round!";
     console.log("player gets point. total: " + playerScore);
   } else if (computerSelection === "rock" && playerSelection === "scissors") {   
     compScore++;
+    result.textContent = "Computer wins round!";
     console.log("comp gets point. total: " + compScore);
   } else if (computerSelection === "paper" && playerSelection === "rock") {
     compScore++;
+    result.textContent = "Computer wins round!";
     console.log("comp gets point. total: " + compScore);
   } else if (computerSelection === "scissors" && playerSelection === "paper") {
     compScore++;
+    result.textContent = "Computer wins round!";
     console.log("comp gets point. total: " + compScore);
   } 
 }
 
-function game() {
-  while (playerScore !== 3 || compScore !== 3) {
-    playRound(prompt("Enter rock paper or scissors"), getComputerChoice());
-
-    if (playerScore === 3) {
-      console.log("player wins!");
-      break;
-    } else if (compScore === 3) {
-      console.log("comp wins!");
-      break;
-    }
-  }
-
-
+function endGame() {
+  gameEnded = true;
+  result.textContent = "Game over."
+  but1.removeEventListener("click", playRock);
+  but2.removeEventListener("click", playPaper)
+  but3.removeEventListener("click", playScissors);
 }
 
-game();
+function playRock() {
+  if (!gameEnded) {
+    playRound("rock", getComputerChoice());
+    score1.textContent = "Player: " + playerScore;
+    score2.textContent = "Computer: " + compScore;
+  }
+  if (playerScore === 3 || compScore == 3) {
+    endGame();
+  }
+}
+
+function playPaper() {
+  if (!gameEnded) {
+    playRound("paper", getComputerChoice());
+    score1.textContent = "Player: " + playerScore;
+    score2.textContent = "Computer: " + compScore;
+  }
+  if (playerScore === 3 || compScore == 3) {
+    endGame();
+  }
+}
+
+function playScissors() {
+  if (!gameEnded) {
+    playRound("scissors", getComputerChoice());
+    score1.textContent = "Player: " + playerScore;
+    score2.textContent = "Computer: " + compScore;
+  }
+  if (playerScore === 3 || compScore == 3) {
+    endGame();
+  }
+}
+
+
+
+//GAME UI AND MORE LOGIC 
+const theDiv = document.getElementById("selections");
+const theOtherDiv = document.getElementById("score-display");
+
+const but1 = document.createElement("button");
+const but2 = document.createElement("button");
+const but3 = document.createElement("button");
+
+but1.textContent = "Rock";
+but2.textContent = "Paper";
+but3.textContent = "Scissors";
+
+theDiv.appendChild(but1);
+theDiv.appendChild(but2);
+theDiv.appendChild(but3);
+
+
+const score1 = document.createElement("p");
+const score2 = document.createElement("p");
+const result = document.createElement("p");
+
+score1.textContent = "Player: " + playerScore;
+score2.textContent = "Computer: " + compScore;
+
+theOtherDiv.appendChild(score1);
+theOtherDiv.appendChild(score2);
+theOtherDiv.appendChild(result);
+
+
+but1.addEventListener("click", playRock);
+but2.addEventListener("click", playPaper)
+but3.addEventListener("click", playScissors);
